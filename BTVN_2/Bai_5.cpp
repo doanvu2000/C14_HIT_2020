143943dceed53128910447df635cc87685d1c0fc
Vũ Văn Doan <55150896+doanvu2000@users.noreply.github.com>
2020-05-10 20:36:45 +0700

    Update Bai_5.cpp

--- BTVN_2/Bai_5.cpp
+++ BTVN_2/Bai_5.cpp
@@ -6,7 +6,10 @@ using namespace std;
 https://viblo.asia/p/so-chu-so-0-lien-tiep-cuoi-cung-cua-n-4P856XOGZY3
 */
 
-//C1:
+//C1: Giống cách của chị Yến nhé mn :D , Thay vì phân tích các số ra thừa số nguyên tố rồi đếm các cặp số (2;5)
+// Thì ta thấy số 5 lúc nào cũng xuất hiện ít hơn => ta chỉ cần đi đếm số 5 là được.
+//VD: 10! = 3,628,800‬ => có 2 chữ số 0 tận cùng
+// 10 = 1 *2*3*4*5*6*7*8*9*10 => số 5 sẽ xuất hiện 2 lần ở 5 và 10.
 int count(long long n){
 	int i = 5,result = 0,j;
 	while(i<=n){		
